bf46f052a80c83fe2d396bb886464a8cd2739259
edunola13 <edunola13@hotmail.com>
2015-09-28 13:32:59 -0300

    Google analytics

--- aplication/source/view/sections/head.php
+++ aplication/source/view/sections/head.php
@@ -13,4 +13,15 @@
 <link rel="shortcut icon" href="<?php echo base();?>resources/images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="<?php echo base();?>resources/images/favicon.ico" type="image/x-icon">
    
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51703505-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+   
 <?php i18n('languaje', LOCALE_URI); ?>
